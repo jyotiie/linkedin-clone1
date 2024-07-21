6dd6ce803e69e011c0b4b8576fa05bbb430afac1
Jyoti <singlajyoti05@gmail.com>
2024-07-21 13:51:40 +0530

    timepass

---
+++ Client/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import './App.css';
+import Login from "./components/Login";
+import Home from './components/Home';
+import Header from './components/Header';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Router>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route path='/home'>
+          <Header/>
+          <Home />
+        </Route>
+      </Switch>
+     </Router>
+    </div>
+  );
+}
+
+export default App;
